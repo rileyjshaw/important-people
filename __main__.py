5980a9bb3e76541ef354235d264fe68b0783ed5b
Riley Shaw <rileyjshaw@gmail.com>
2014-04-19 19:31:54 -0400

    v0.1.0

--- __main__.py
+++ __main__.py
@@ -1,5 +1,11 @@
-from BeautifulSoup import BeautifulSoup
+import io, json
+from datetime import datetime
+from bs4 import BeautifulSoup
 from urllib2 import urlopen
+
+sentinel = object()
+startTime = datetime.now()
+
 wiki_base = 'http://en.wikipedia.org'
 list_base = '/w/index.php?title=Category:'
 births = '_births'
@@ -15,58 +21,63 @@ important_people = {
 def test():
     albert = describe_person('http://en.wikipedia.org/wiki/Einstein')
     if albert[0] != 'Albert Einstein':
-        print 'describe_person failed. Expected "Albert Einstein", got ' + str(albert[0]) + '"'
+        print 'describe_person failed. Expected "Albert Einstein", got "' + unicode(albert[0]) + '"'
     if type(albert[1]) is not int:
-        print 'describe_person failed. Expected an int, got ' + str(albert[1])
+        print 'describe_person failed. Expected an int, got ' + unicode(albert[1])
 
 
 def count_words(soup):
-    words = soup.getText()
-    count = 0
-    for word in words.split()
-        count
-    # print 'count_words is not ready yet :('
-    return 404
+    words = soup.get_text()
+    return len(words.split())
 
 
 def describe_person(url):
     soup = BeautifulSoup(urlopen(url))
     page_content = soup.find(id = 'content')
-    name = page_content.find('h1', id = 'firstHeading').getText()
+    name = page_content.find('h1', id = 'firstHeading').get_text()
     words = count_words(page_content.find(id = 'mw-content-text'))
-    print [name, words]
+    #name = name.encode('utf-8')
+    print 'Done ' + name
     return [name, words]
 
 
 def scrape_list(url, results):
-    if url is False:
-        return results
-
     soup = BeautifulSoup(urlopen(url))
     page_content = soup.find('div', id = 'mw-pages')
 
     try:
-        next_page = page_content.find('a', text = 'next 200').parent.get('href')
+        next_page = page_content.find('a', text = 'next 200').get('href')
     except AttributeError as e:
         next_page = False
 
-    for column in page_content.findAll('td'):
-        for link in column.findAll('a'):
+    for column in page_content.find_all('td'):
+        for link in column.find_all('a'):
             results.append(describe_person(wiki_base + link.get('href')))
 
-    return scrape_list(next_page, results)
+    if next_page:
+        return scrape_list(wiki_base + next_page, results)
+    else:
+        return results
 
 
 def organize():
     for year, people in important_people['births'].items():
-        people.sort(key = lambda x: x[1])
+        people.sort(key = lambda x: x[1], reverse = True)
 
 
-def run(first, last):
+def run(first, last = sentinel):
+    if last is sentinel:
+        last = first
+
     for year in range(first, last + 1):
-        important_people['births'][year] = scrape_list(wiki_base + list_base + str(year) + births, [])
+        important_people['births'][year] = scrape_list(wiki_base + list_base + unicode(year) + births, [])
+
     organize()
 
+    with io.open(unicode(first) + '-' + unicode(last) + '-births.json', 'w', encoding='utf-8') as f:
+      f.write(unicode(json.dumps(important_people, ensure_ascii = False)))
 
-# test()
-run(1900, 1980)
+
+run(1987)
+
+print 'Completed in ' + unicode(datetime.now() - startTime)
